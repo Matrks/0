92329cee6d4531ff5ffd491539469d536a665b37
Matrks <45499570+Matrks@users.noreply.github.com>
2019-02-03 16:49:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1302,7 +1302,7 @@ if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'move')) {
  if (message.member.hasPermission("MOVE_MEMBERS")) {
  if (message.mentions.users.size === 0) {
- return message.channel.send("**لاستخدام الأمر اكتب هذه الأمر : **"** +prefix+ "move [USER]**")
+ return message.channel.send("`لاستخدام الأمر اكتب هذه الأمر : `" +prefix+ "`move [USER]`")
 }
 if (message.member.voiceChannel != null) {
  if (message.mentions.members.first().voiceChannel != null) {
@@ -1311,7 +1311,7 @@ if (message.member.voiceChannel != null) {
 var embed = new Discord.RichEmbed()
  .setTitle("Succes!")
  .setColor("#000000")
- .setDescription(`**لقد قمت بسحب <@${usermentioned}> الى الروم الصوتي الخاص بك✅ **`)
+ .setDescription(`لقد قمت بسحب <@${usermentioned}> الى الروم الصوتي الخاص بك✅ `)
 var embed = new Discord.RichEmbed()
 .setTitle(`You are Moved in ${message.guild.name}`)
  .setColor("RANDOM")
@@ -1319,10 +1319,10 @@ var embed = new Discord.RichEmbed()
  message.guild.members.get(usermentioned).setVoiceChannel(authorchannel).then(m => message.channel.send(embed))
 message.guild.members.get(usermentioned).send(embed)
 } else {
-message.channel.send("**لا تستطيع سحب **"+ message.mentions.members.first() +" **يجب ان يكون هذه العضو في روم صوتي**")
+message.channel.send("`لا تستطيع سحب `"+ message.mentions.members.first() +"`يجب ان يكون هذه العضو في روم صوتي`")
 }
 } else {
- message.channel.send("**يجب ان تكون في روم صوتي لكي تقوم بسحب العضو أليك**")
+ message.channel.send("`يجب ان تكون في روم صوتي لكي تقوم بسحب العضو أليك`")
 }
 } else {
 message.react("❌")
