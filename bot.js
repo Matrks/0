7770909bdb0ab76619f7ff1f2d8201e516e5cc40
Matrks <45499570+Matrks@users.noreply.github.com>
2019-02-14 22:53:45 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1697,4 +1697,15 @@ eyad.delete();
 }}
 });
 
+    client.on('message', message => {
+   if(message.content.startsWith(prefix + "invite")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} has ${inviteCount} invites.`);
+});
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
