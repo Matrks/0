1dfb763358adde76e37aa81d196cc9b8816cc1fe
Matrks <45499570+Matrks@users.noreply.github.com>
2019-02-15 17:14:38 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1706,8 +1706,7 @@ const getYouTubeID = require("get-youtube-id");
 const fetchVideoInfo = require("youtube-info");
 const simpleytapi = require('simple-youtube-api')
 const yt_api_key = "AIzaSyDoH9YxF0yi6ljyi2txYZHB10vXNUEP_2U"
-const prefix = "m-";
-client.login(process.env.SECERT_KEY);
+client.login(process.env.BOT_TOKEN);
 var guilds = {};
 /////////////////////////////////////////////////////////////////
 client.on('error', (err) => console.log(err));
