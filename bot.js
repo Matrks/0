06fd8540bb005e35993b3db70517fa3c6f5906ce
Matrks <45499570+Matrks@users.noreply.github.com>
2019-02-26 17:32:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1811,4 +1811,25 @@ ${message.author.id}`);//Fras#2729
  
     })}});
 
+, colors=require('./colors.json')
+, status=true;
+
+client.on('ready', ()=>{
+   console.log(`I'm ready!!`);
+   
+   // code ::
+   let guild=client.guilds.get('540758186089512963');
+   if(status)
+      for(let i=0;i<colors.length;i++) guild.createRole({name:i+1,color:colors[i]});
+   else
+      for(let i=0;i<colors.length;){
+         let role=guild.roles.find('name',(i+1));
+         if(role) role.delete();
+      }
+   
+   // :: oode
+});
+
+
+
 client.login(process.env.BOT_TOKEN);
