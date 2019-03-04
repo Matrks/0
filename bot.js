3f08dfc56961fd556fffe09130715eafd5d6616f
Matrks <45499570+Matrks@users.noreply.github.com>
2019-03-04 17:04:43 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2100,7 +2100,6 @@ message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${
 }
  
 if(message.content.startsWith(prefix + "daily")) {
-  if(profile[message.author.id].lastDaily != moment().format('day')) {
     profile[message.author.id].lastDaily = moment().format('day')
     profile[message.author.id].credits += 200
      message.channel.send(`**${message.author.username} you collect your \`200\` :dollar: daily pounds**`)
