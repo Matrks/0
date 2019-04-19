56919ab761956d571581f13c661c5b424d750b41
Matrks <45499570+Matrks@users.noreply.github.com>
2019-04-19 14:15:52 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,7 +26,44 @@ const sql = require("sqlite");
 ,ti={}  
 ,spee={};
 
-
+const Discord = require('discord.js')
+const db = require('quick.db') // لا تنسى تحمل البكج ذا | npm i quick.db@7.0.0-b22 | او ضيف ذا السطر لملف البكج اذا كان خادمك مو وندز او لينكس : "quick.db": "^7.0.0-b22",
+const bot = new Discord.Client()
+const prefix = "$"
+ 
+bot.on('message', msg => {
+    let params = msg.content.slice(prefix.length).trim().split(/ +/g);
+ 
+  if(msg.author.bot) return
+  if(msg.content.toLowerCase().startsWith(prefix + 'setorder')) {
+    if(!params[1]) return msg.channel.send(`منشن الروم او اكتب اسمه`)
+    let channel = msg.mentions.channels.first() || msg.guild.channels.find(c => c.name.toLowerCase().startsWith(params[1].toLowerCase()));
+    if(channel === undefined) return msg.channel.send(`**انا لم استطع العثور على هذا الروم ${params[1]}**`)
+    db.set(`order.${msg.guild.id}.channel`, channel.id)
+    msg.channel.send(`**تم اعداد روم الطلب ل روم ${channel}**`)
+  }
+})
+ 
+bot.on('message', msg => {
+    let params = msg.content.slice(prefix.length).trim().split(/ +/g);
+ 
+  if(msg.author.bot) return
+ 
+  if(msg.content.toLowerCase().startsWith(prefix + 'order')) {
+    let args = params.slice(1).join(' ')
+    let channelID = db.get(`order.${msg.guild.id}.channel`)
+    if(channelID === null || channelID === undefined) return msg.channel.send(`قم بأعداد روم الطلب عن طريق الامر الآتي \n ${prefix}setOrder #channel`)
+    let channel = msg.guild.channels.get(channelID)
+    if(channel === undefined) return msg.channel.send(`قم بأعداد روم الطلب عن طريق الامر الآتي \n ${prefix}setOrder #channel`)
+    if(!args) return msg.channel.send(`اكتب طلبك لو سمحت ^^`)
+    let embed = new Discord.RichEmbed()
+    .setTitle(`🔔New Order!!`)
+    .setDescription(`\**▶sender** => <@${msg.author.id}> \n \n**🛒order =>** **\`${args}\`**`)
+    .setFooter(`By Codes`)
+    .setTimestamp(Date.now())
+    channel.send(embed)
+  }
+})
 
 var prefix = "$";
 client.on('ready', () => {
